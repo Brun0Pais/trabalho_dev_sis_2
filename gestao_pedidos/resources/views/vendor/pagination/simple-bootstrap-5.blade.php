92de61a38ef4782baa75b3250e0744573953baaa
Arthur-Lunardi <arthur300107@gmail.com>
2025-11-13 21:07:20 -0300

    a

---
+++ gestao_pedidos/resources/views/vendor/pagination/simple-bootstrap-5.blade.php
@@ -0,0 +1,36 @@
+@if ($paginator->hasPages())
+    <nav>
+        <ul class="pagination justify-content-center">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span class="page-link" aria-hidden="true">
+                        <i class="bi bi-chevron-left"></i>
+                    </span>
+                </li>
+            @else
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
+                        <i class="bi bi-chevron-left"></i>
+                    </a>
+                </li>
+            @endif
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
+                        <i class="bi bi-chevron-right"></i>
+                    </a>
+                </li>
+            @else
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span class="page-link" aria-hidden="true">
+                        <i class="bi bi-chevron-right"></i>
+                    </span>
+                </li>
+            @endif
+        </ul>
+    </nav>
+@endif
+
